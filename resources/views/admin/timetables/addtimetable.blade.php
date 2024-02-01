c89cc585316a1358d96206da160d25404a341c1b
mjunaidrafique <asadahmad.ia5192@gmail.com>
2024-02-01 23:52:44 +0500

    jhk

--- resources/views/admin/timetables/addtimetable.blade.php
+++ resources/views/admin/timetables/addtimetable.blade.php
@@ -95,7 +95,7 @@
 				<label for="inputPassword5" class="form-label">Password</label>
 				<input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock">
 			</div>
-		</div>
+		</div>hhgh
 	</form>
 </div>
 @endsection
